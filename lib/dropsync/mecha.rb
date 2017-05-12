3ca1da1a60302f4f12d4fd3a38ca043faa5e8c35
Frank Kair <frankkair@gmail.com>
2017-05-12 18:21:53 -0300

    Adds comments and uses FileUtils rather than system() to remove .zip file

--- lib/dropsync/mecha.rb
+++ lib/dropsync/mecha.rb
@@ -9,8 +9,11 @@ module DropSync
           agent.user_agent_alias = 'Mac Safari'
         end
 
+        # Gets download link
         page = agent.get(url)
         link = page.uri.to_s.gsub('dl=0', 'dl=1')
+
+        # Creates directory
         download_path = "#{File.expand_path('~')}/Downloads"
         FileUtils.cd(download_path)
         path.split('/').each do |d|
@@ -23,13 +26,15 @@ module DropSync
           end
           FileUtils.cd(d)
         end
-
         filename = path.split('/').pop
 
+        # Download
         system("curl -L -s -o #{download_path}/#{path}/#{filename}.zip #{link}")
         system("unzip #{download_path}/#{path}/#{filename}.zip > /dev/null 2>&1")
+
+        # Cleans directory
         FileUtils.rm_rf("__MACOSX")
-        system("rm #{filename}.zip")
+        FileUtils.rm_rf("#{filename}.zip")
     end
   end
 end
