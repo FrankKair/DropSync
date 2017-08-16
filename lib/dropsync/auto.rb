a9b7935075d6de3a27a07a93507a71d8792398a6
Frank Kair <frankkair@gmail.com>
2017-08-15 21:53:33 -0300

    Tabs -> Spaces (2)

--- lib/dropsync/auto.rb
+++ lib/dropsync/auto.rb
@@ -2,39 +2,39 @@ require 'mechanize'
 require 'fileutils'
 
 module DropSync
-    module Auto
-    	extend self
-        def download(path, url)
-            agent = Mechanize.new do |agent|
-                agent.user_agent_alias = 'Mac Safari'
-            end
+  module Auto
+    extend self
+    def download(path, url)
+      agent = Mechanize.new do |agent|
+        agent.user_agent_alias = 'Mac Safari'
+      end
 
-            # Gets download link
-            page = agent.get(url)
-            link = page.uri.to_s.gsub('dl=0', 'dl=1')
+      # Gets download link
+      page = agent.get(url)
+      link = page.uri.to_s.gsub('dl=0', 'dl=1')
 
-            # Creates directory
-            download_path = "#{File.expand_path('~')}/Downloads"
-            FileUtils.cd(download_path)
-            path.split('/').each do |d|
-                begin
-                    FileUtils.mkdir(d)
-                rescue
-                    puts "> Directory '#{d}' already exists"
-                    FileUtils.rm_rf(d)
-                    FileUtils.mkdir(d)
-                end
-                FileUtils.cd(d)
-            end
-            filename = path.split('/').pop
-
-            # Download
-            system("curl -L -s -o #{download_path}/#{path}/#{filename}.zip #{link}")
-            system("unzip #{download_path}/#{path}/#{filename}.zip > /dev/null 2>&1")
-
-            # Cleans directory
-            FileUtils.rm_rf("__MACOSX")
-            FileUtils.rm_rf("#{filename}.zip")
+      # Creates directory
+      download_path = "#{File.expand_path('~')}/Downloads"
+      FileUtils.cd(download_path)
+      path.split('/').each do |d|
+        begin
+          FileUtils.mkdir(d)
+        rescue
+          puts "> Directory '#{d}' already exists"
+          FileUtils.rm_rf(d)
+          FileUtils.mkdir(d)
         end
+        FileUtils.cd(d)
+      end
+      filename = path.split('/').pop
+
+      # Download
+      system("curl -L -s -o #{download_path}/#{path}/#{filename}.zip #{link}")
+      system("unzip #{download_path}/#{path}/#{filename}.zip > /dev/null 2>&1")
+
+      # Cleans directory
+      FileUtils.rm_rf("__MACOSX")
+      FileUtils.rm_rf("#{filename}.zip")
     end
+  end
 end
--- lib/dropsync/engine.rb
+++ lib/dropsync/engine.rb
@@ -1,44 +1,43 @@
 module DropSync
-	class Engine
-        def initialize(access_token)
-            @client = DropboxClient.new(access_token)
-        end
-
-        def download(path)
-            puts '--- DropSync ---'
-            puts "> Searching for #{path}"
-            url = get_url(path)
-            puts '> Downloading file'
-            Auto::download(path, url)
-            puts '> Download finished'
-            logout
-        end
-
-        def upload(path)
-            puts '--- DropSync ---'
-            filename = File.basename(path_to_file)
-            puts "> Uploading #{filename}"
-            @client.put_file(filename, open(path_to_file))
-            puts '> Upload finished'
-            logout
-        end
-
-        private
-        def get_url(path)
-            filename = path.split('/').pop
-            resp = @client.search('/', filename)
-
-            for it in resp
-            	if it['path'].downcase.include? path.downcase
-            		item_path = it['path']
-            	end
-            end
-            @client.shares(item_path)['url']
-        end
-
-        def logout
-            @client = nil
-            exit(0)
-        end
+  class Engine
+    def initialize(access_token)
+      @client = DropboxClient.new(access_token)
     end
+
+    def download(path)
+      puts '--- DropSync ---'
+      puts "> Searching for #{path}"
+      url = get_url(path)
+      puts '> Downloading file'
+      Auto::download(path, url)
+      puts '> Download finished'
+      logout
+    end
+
+    def upload(path)
+      puts '--- DropSync ---'
+      filename = File.basename(path_to_file)
+      puts "> Uploading #{filename}"
+      @client.put_file(filename, open(path_to_file))
+      puts '> Upload finished'
+      logout
+    end
+
+    private
+    def get_url(path)
+      filename = path.split('/').pop
+      resp = @client.search('/', filename)
+      for it in resp
+        if it['path'].downcase.include? path.downcase
+          item_path = it['path']
+        end
+      end
+      @client.shares(item_path)['url']
+    end
+
+    def logout
+      @client = nil
+      exit(0)
+    end
+  end
 end
