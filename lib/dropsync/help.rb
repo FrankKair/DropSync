8c1e9b03f95c9c5aea3e915ba34b1a4a75204e75
Frank Kair <frankkair@gmail.com>
2017-12-13 11:50:36 -0200

    Modifies bin and help messages removing upload

--- lib/dropsync/help.rb
+++ lib/dropsync/help.rb
@@ -4,22 +4,10 @@ module DropSync
 
     def show_message
       puts '--- DropSync ---'
-      puts '> To use DropSync you should provide an access_token, a command and a file, like so:'
-      puts '$ dropsync access_token command file'
-      puts "\n> Commands:"
-      puts '> download argument -> file/folder'
-      puts '> upload argument -> path to file'
+      puts '> To use DropSync you should provide an access_token and a file/folder, like so:'
       puts "\n> Examples:"
-      puts '$ dropsync access_token download my_dropbox_folder'
-      puts '$ dropsync access_token upload ~/Desktop/my_stuff/my_file.txt'
-      puts '-------'
-    end
-
-    def invalid_command
-      puts '--- DropSync ---'
-      puts '> Please enter a valid command: download or upload'
-      puts '> Need help?'
-      puts '$ dropsync help'
+      puts '$ dropsync access_token file'
+      puts '$ dropsync access_token my_dropbox_folder'
       puts '-------'
     end
   end
